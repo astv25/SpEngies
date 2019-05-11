941a8420743edcc7460ded7c418001e60878f819
astv25 <astv2.5@gmail.com>
2019-05-11 03:31:30 -0500

    Follow Drone
    [sustained screaming]

--- FollowDrone_Atmo/FollowDrone_Atmo/Program.cs
+++ FollowDrone_Atmo/FollowDrone_Atmo/Program.cs
@@ -23,21 +23,21 @@ namespace IngameScript
         public Dictionary<string, bool> alerts = new Dictionary<string, bool>();
         public Dictionary<string, float> values = new Dictionary<string, float>();
         public Dictionary<string, float> valuesold = new Dictionary<string, float>();
-        public const string namebase = "FD001-"; //naming prefix for blocks we care about (e.g.: FD001-Reactor 1)
-        //public List<IMyTerminalBlock> selfgrid = new List<IMyTerminalBlock>();
         public List<IMyTerminalBlock> selfgridgiveashit = new List<IMyTerminalBlock>();
-        public List<string> wantedtypes = new List<string>() { "IMyGasTank", "IMyGyro", "IMyBatteryBlock", "IMyReactor", "IMyBlockGroup", "IMyRadioAntenna", "IMyRemoteControl", "IMyThrust", "IMyLandingGear", "IMySensorBlock" };
-        public List<string> coreTypes = new List<string>() {"IMyBatteryBlock", "IMyReactor", "IMyGasTank"};
-        public List<string> secondaryTypes = new List<string>() { "IMyRadioAntenna", "IMyGyro", "IMySensorBlock", "IMyThrust", "IMyLandingGear", "IMyRemoteControl" };
+        #region mdk preserve
+        public const string namebase = "FD001-"; //naming prefix for blocks we care about (e.g.: FD001-Reactor 1)
         public int minAlt = 20; //Minimum altitude to maintain (meters)
         public int minUr = 1;  //Minimum number of uranium ingots in system
         public const int damageCheckInterval = 5; //How many cycles do we wait between each run of damageCheck()?
         public int tdCI = 0; //Counter for damageCheck interval
-        public string teltag = "FollowDrone"; //Tag to identify valid broadcast targets, apparently.  Thanks, Keen
+        //public string teltag = "FollowDrone"; //Tag to identify valid broadcast targets, apparently.  Thanks, Keen
         public int divergenceThreshold = 20; //how much valuesold and values can diverge negatively
         public const double CTRL_COEFF = 0.3; //defines the strength of Pitch/Roll/Yaw correction
-
-        public Program() { Runtime.UpdateFrequency = UpdateFrequency.Update10; }
+        public const bool damageCare = false; //do we give a shit about damage?  if true, drone will attempt landing if damaged
+        public float thrustoverride = 0.25f; //percentage of thruster override
+        public int altMargin = 5; //minAlt +/- altMargin gives drone target altitude range
+        #endregion
+        public Program() { Runtime.UpdateFrequency = UpdateFrequency.Update10;}
 
         public void Main()
         {
@@ -46,9 +46,19 @@ namespace IngameScript
             try
             {
                 checkCore();
-                if (tdCI == damageCheckInterval) { damageCheck(); tdCI = 0;}
-                else { tdCI++;}
+                if (damageCare)
+                {
+                    //DEBUG:
+                    Echo("Running damage checks...");
+                    Echo("Damage check counter is " + tdCI);
+                    if (tdCI == damageCheckInterval) { damageCheck(); tdCI = 0; }
+                    else { tdCI++; }
+                }
+                //DEBUG:
+                Echo("Running positional checks...");
                 checkPos();
+                //DEBUG:
+                Echo("Running owner proximity check...");
                 checkProx();
             }
             catch (LandingException ex)
@@ -61,7 +71,7 @@ namespace IngameScript
                 Echo("Unknown error, landing!");
                 land();
             }
-
+           
         }
         public void initList()
         {
@@ -76,17 +86,7 @@ namespace IngameScript
                 }
             }
             //Init block lists
-            if (selfgridgiveashit.Count == 0)
-            {
-                foreach (string kind in wantedtypes)
-                {
-                    List<IMyTerminalBlock> tmpall = new List<IMyTerminalBlock>();
-                    GridTerminalSystem.GetBlocks(tmpall);
-                    foreach(IMyTerminalBlock block in tmpall) { if (block.BlockDefinition.TypeIdString == kind) { selfgridgiveashit.Add(block);}}
-                }
-
-            }
-
+            if (selfgridgiveashit.Count == 0) { GridTerminalSystem.GetBlocks(selfgridgiveashit);}
         }
         public class LandingException : Exception
         {
@@ -99,143 +99,158 @@ namespace IngameScript
         public void checkCore()
         {
             //Store previous values
-            foreach (KeyValuePair<string, float> kvp in values)
-            {
-                valuesold[kvp.Key] = kvp.Value;
-            }
-            /*{ valuesold[kvp] = values[kvp]; }*/
+            foreach (KeyValuePair<string, float> kvp in values) { valuesold[kvp.Key] = kvp.Value;}
+            //Get velocity
+            IMyRemoteControl RC;
+            var tRC = new List<IMyRemoteControl>();
+            GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(tRC);
+            RC = tRC[0];
+            var velocity = RC.GetShipSpeed();
+            Echo("Vel: " + (int)velocity + "m/s");
+            //Get altitude
+            double altitude;
+            RC.TryGetPlanetElevation(MyPlanetElevation.Surface, out altitude);
+            Echo("Alt: " + (int)altitude + "m");
             //Get H2 tank fill average
             float tgas = 0.0f;
             int tgascnt = 0;
-            foreach (IMyGasTank tank in selfgridgiveashit)
-            {
-                if (!tank.Name.Contains("Reserve"))
-                {
-                    tgas += (float)tank.FilledRatio * 100;
-                    tgascnt++;
-                }
-            }
+            var ttanks = new List<IMyGasTank>();
+            GridTerminalSystem.GetBlocksOfType(ttanks);
+            foreach (IMyGasTank tank in ttanks) { if(tank.DisplayNameText.Contains("H2") || tank.DisplayNameText.Contains("Hydrogen")) { if (!tank.DisplayNameText.Contains("Reserve")) { tgas += (float)tank.FilledRatio * 100; tgascnt++; } } }
             tgas = tgas / tgascnt; //Average out total fill %
             values[careabout[0]] = tgas;
-            if (tgas <= 25)
-            { engageReserves(); throw new LandingException("Bingo fuel"); }
+            Echo("H2: " + tgas + "%");
+            if (tgas <= 25) { engageReserves(); throw new LandingException("On fuel reserves"); }
             //Get battery charge average
             float tbchg = 0.0f;
             int tbcnt = 0;
-            foreach (IMyBatteryBlock batt in selfgridgiveashit)
+            var tbatts = new List<IMyBatteryBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(tbatts);
+            foreach (IMyBatteryBlock batt in tbatts)
             { tbchg += batthelper(batt); tbcnt++; }
             tbchg = tbchg / tbcnt; //Average out total battery charge %
             values[careabout[2]] = tbchg;
-            if (tbchg <= 25)
-            {
-                throw new LandingException("Low battery");
-            }
+            Echo("Battery: " + tbchg + "%");
+            if (tbchg <= 25) { throw new LandingException("Low battery"); }
             //Get Uranium ingots left in reactor(s)
             long tUr = 0;
             double tUrI = 0;
-            foreach (IMyReactor react in selfgridgiveashit) {
-                List<MyInventoryItem> items = new List<MyInventoryItem>();
+            var treact = new List<IMyReactor>();
+            GridTerminalSystem.GetBlocksOfType<IMyReactor>(treact);
+            foreach (IMyReactor react in treact) {
+                var items = new List<MyInventoryItem>();
                 react.GetInventory().GetItems(items);
                 foreach (MyInventoryItem itm in items) { tUr += itm.Amount.RawValue; } }
             tUrI = Math.Round(((double)tUr / 1000000), 2);
             values[careabout[1]] = (float)tUrI;
-            if (tUrI < minUr)
-            {
-                throw new LandingException("Uranium low");
-            }
+            //DEBUG:  show calculated uranium amount
+            Echo("Total Uranium: " + (float)tUrI + "kg");
+            if (tUrI < minUr) { throw new LandingException("Uranium low");}
             //Compare current values to previous
             foreach (string check in careabout)
             {
                 float tcheck = valuesold[check] - values[check];
-                if (tcheck >= 0)
-                {
-                    if (tcheck >= divergenceThreshold)
-                    {
-                        throw new LandingException(string.Format("Value of {0} exceeded negative change threshold", check));
-                    }
-                }
+                if (tcheck >= 0) { if (tcheck >= divergenceThreshold) { throw new LandingException(string.Format("Value of {0} exceeded negative change threshold", check));}}
             }
             //Send telemetry home
-            sendTelemetry(values);
+            //sendTelemetry(values);
             return;
         }
         public void checkPos()
         {
             correctOrient();
+            //DEBUG:
+            Echo("Correcting altitude...");
             correctAlt();
         }
         public void correctAlt()
         {
             //Get remote control
             IMyRemoteControl RC;
-            List<IMyRemoteControl> tRC = new List<IMyRemoteControl>();
+            //DEBUG:
+            Echo("-Getting remote control...");
+            var tRC = new List<IMyRemoteControl>();
             GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(tRC);
             RC = tRC[0]; //fuck me, that's retarded
+            //DEBUG:
+            Echo("-Getting list of lift thrusters...");
             //Get applicable thrusters
-            List<IMyThrust> lift = new List<IMyThrust>();
-            List<IMyThrust> tlift = new List<IMyThrust>();
-            GridTerminalSystem.GetBlocksOfType<IMyThrust>(tlift);
-            foreach (IMyThrust thruster in tlift) { if (thruster.Orientation.Forward.ToString() == "Up") { lift.Add(thruster); } }
+            var lifts = new List<IMyThrust>();
+            GridTerminalSystem.GetBlocksOfType<IMyThrust>(lifts, lift => lift.Orientation.Forward.ToString() == "Down");
             //Try to keep drone at minAlt
             //TODO:  set thrust override based on velocity AND total mass
-            float thrustoverride = 1.0f;
+            //DEBUG:
+            Echo("-Beginning altitude correction with " + lifts.Count + " thrusters...");
             double altitude;
             RC.TryGetPlanetElevation(MyPlanetElevation.Surface, out altitude);
-            while (altitude <= minAlt)
+            if(altitude <= minAlt - altMargin)
             {
-                //slowly reduce thrust override until we're balanced
-                //hopefully, anyway
-                while (RC.GetShipSpeed() >= 0) { foreach (IMyThrust thruster in lift) { thruster.Enabled = true; thruster.ThrustOverride = thrustoverride; } thrustoverride -= 0.05f; }
+                //DEBUG:
+                Echo("-Altitude is too low");
+                RC.DampenersOverride = false;
+                if (RC.GetShipSpeed() <= 5) { foreach (IMyThrust thruster in lifts) { thruster.ThrustOverridePercentage = thrustoverride; thruster.Enabled = true; } }
+                else if (RC.GetShipSpeed() > 5) { foreach (IMyThrust thruster in lifts) { thruster.ThrustOverridePercentage = 0; } }
             }
-            while (altitude > minAlt)
+            else if (altitude > minAlt + altMargin)
             {
-                while (RC.GetShipSpeed() <= 5) { foreach (IMyThrust thruster in lift) { thruster.Enabled = false; } } //cut lift thrust
-                foreach (IMyThrust thruster in lift) { thruster.Enabled = true; } //moderate speed
+                //DEBUG:
+                Echo("-Altitude is too high");
+                RC.DampenersOverride = false;
+                if(RC.GetShipSpeed() <= 5) { foreach (IMyThrust thruster in lifts) { thruster.ThrustOverridePercentage = 0; thruster.Enabled = false; } }//cut lift thrusters
+               else if(RC.GetShipSpeed() > 5) { foreach (IMyThrust thruster in lifts) { thruster.ThrustOverridePercentage = thrustoverride; thruster.Enabled = true; } }//moderate speed
+            }
+            else if ((minAlt+altMargin) > altitude && altitude > (minAlt - altMargin))
+            {
+                //DEBUG:
+                Echo("-Altitude is ok");
+                foreach (IMyThrust thruster in lifts) { thruster.ThrustOverridePercentage = 0;thruster.Enabled = true; }
+                RC.DampenersOverride = false;
             }
         }
         public void correctOrient()
         {
             //Get remote control
             IMyRemoteControl RC;
-            List<IMyRemoteControl> tRC = new List<IMyRemoteControl>();
+            var tRC = new List<IMyRemoteControl>();
             GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(tRC);
             RC = tRC[0]; //still retarded
-            //Get gyro
-            IMyGyro mGy;
-            List<IMyGyro> tmGy = new List<IMyGyro>();
-            GridTerminalSystem.GetBlocksOfType<IMyGyro>(tmGy);
-            mGy = tmGy[0];
+            //Get gyros
+            var tmGy = new List<IMyGyro>();
+            GridTerminalSystem.GetBlocksOfType<IMyGyro>(tmGy);            
             //Check pitch/roll/yaw compared to gravity
             Matrix orient;
             RC.Orientation.GetMatrix(out orient);
             Vector3D down = orient.Down;
             Vector3D grav = RC.GetNaturalGravity();
             grav.Normalize();
-            mGy.Orientation.GetMatrix(out orient);
-            var lDown = Vector3D.Transform(down, MatrixD.Transpose(orient));
-            var lGrav = Vector3D.Transform(grav, MatrixD.Transpose(mGy.WorldMatrix.GetOrientation()));
-            var rot = Vector3D.Cross(lDown, lGrav);
-            double ang = rot.Length();
-            ang = Math.Atan2(ang, Math.Sqrt(Math.Max(00, 1.0 - ang * ang)));
-            if (ang > 0.01)
+            foreach (IMyGyro gyro in tmGy)
             {
-                double ctrl_vel = mGy.GetMaximum<float>("Yaw") * (ang / Math.PI) * CTRL_COEFF;
-                ctrl_vel = Math.Min(mGy.GetMaximum<float>("Yaw"), ctrl_vel);
-                ctrl_vel = Math.Max(0.01, ctrl_vel);
-                rot.Normalize();
-                rot *= ctrl_vel;
-                mGy.SetValueFloat("Pitch", (float)rot.GetDim(0));
-                mGy.SetValueFloat("Yaw", -(float)rot.GetDim(1));
-                mGy.SetValueFloat("Roll", -(float)rot.GetDim(2));
-                mGy.SetValueFloat("Power", 1.0f);
-                mGy.GyroOverride = true;
+                gyro.Orientation.GetMatrix(out orient);
+                var lDown = Vector3D.Transform(down, MatrixD.Transpose(orient));
+                var lGrav = Vector3D.Transform(grav, MatrixD.Transpose(gyro.WorldMatrix.GetOrientation()));
+                var rot = Vector3D.Cross(lDown, lGrav);
+                double ang = rot.Length();
+                ang = Math.Atan2(ang, Math.Sqrt(Math.Max(00, 1.0 - ang * ang)));
+                if (ang > 0.01)
+                {
+                    double ctrl_vel = gyro.GetMaximum<float>("Yaw") * (ang / Math.PI) * CTRL_COEFF;
+                    ctrl_vel = Math.Min(gyro.GetMaximum<float>("Yaw"), ctrl_vel);
+                    ctrl_vel = Math.Max(0.01, ctrl_vel);
+                    rot.Normalize();
+                    rot *= ctrl_vel;
+                    gyro.SetValueFloat("Pitch", (float)rot.GetDim(0));
+                    gyro.SetValueFloat("Yaw", -(float)rot.GetDim(1));
+                    gyro.SetValueFloat("Roll", -(float)rot.GetDim(2));
+                    gyro.SetValueFloat("Power", 1.0f);
+                    gyro.GyroOverride = true;
+                }
+                if (ang < 0.01) { gyro.GyroOverride = false; gyro.SetValueFloat("Pitch", 0); gyro.SetValueFloat("Yaw", 0); gyro.SetValueFloat("Roll", 0); }
             }
-            if (ang < 0.01) { mGy.GyroOverride = false; }
             return;
         }
         public void damageCheck()
         {
-            foreach (IMyTerminalBlock block in selfgridgiveashit) { if (getHealth(block) <= 0.75f) { throw new LandingException(string.Format("Damage to {0}, {1}", block.BlockDefinition.TypeIdString, block.Name)); } } 
+            foreach (IMyTerminalBlock block in selfgridgiveashit) { if (getHealth(block) <= 0.75f) { throw new LandingException(string.Format("Damage to {0}, {1}", block.BlockDefinition.TypeIdString.Split('_')[1], block.Name)); } } 
             return;
         }
         public float getHealth(IMyTerminalBlock block)
@@ -245,22 +260,22 @@ namespace IngameScript
             float buildIntegrity = target.BuildIntegrity;
             float currentDamage = target.CurrentDamage;
             return (buildIntegrity - currentDamage) / maxIntegrity;
-        }
+        }/*
         public void sendTelemetry(Dictionary<string, float> data)
         {
-            //First, horrific antenna fuckery
-            IMyIntergridCommunicationSystem ant;
-            List<IMyIntergridCommunicationSystem> ants = new List<IMyIntergridCommunicationSystem>();
-            foreach (IMyTerminalBlock block in selfgridgiveashit) { if (block.BlockDefinition.TypeIdString == "IMyIntergridCommunicationSystem") { ants.Add(block as IMyIntergridCommunicationSystem); }}
+            IMyRadioAntenna ant;
+            List<IMyRadioAntenna> ants = new List<IMyRadioAntenna>();
+            GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(ants);
+            //DEBUG
+            Echo(ants[0].ToString());
             try { ant = ants[0]; }
             catch (Exception) { throw new LandingException("Antenna bind failure"); }
-            //holy fuck, I hate myself for that
             string message = namebase;
             foreach (string str in careabout)
             { message += " " + str + " " + values[str]; }
-            ant.SendBroadcastMessage(teltag, message);
+            ant.TransmitMessage(message);
             return;
-        }
+        }*/
         public void engageReserves()
         {
             //Allow fuel to be pulled from reserve tanks for landing
